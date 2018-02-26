f7a380817dcee6dc0527ba4e628b964a4c2d6280
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2018-02-26 15:39:24 -0500

    demo gridview and added images

--- ClassManager/app/src/main/java/com/example/camdavejakerob/classmanager/MainActivity.java
+++ ClassManager/app/src/main/java/com/example/camdavejakerob/classmanager/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.camdavejakerob.classmanager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         // we might want to load all of there info on to a local data base so it can run faster
         Intent classesIntent = new Intent(MainActivity.this, ClassActivity.class);
         startActivity(classesIntent);
+
+
     }
 }
