5388a7e3dcbedc56cdd56cfb1da0f364be43f70e
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2018-03-10 18:37:16 -0500

    added images
    Also if you tap the information button on the main activity it bring you to the more info activity. Also updated class list view to reflect the mock

--- ClassManager/app/src/main/java/com/example/camdavejakerob/classmanager/MainActivity.java
+++ ClassManager/app/src/main/java/com/example/camdavejakerob/classmanager/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final LinearLayout button = findViewById(R.id.inforamtion);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent classesIntent = new Intent(MainActivity.this, ClassInfoActivity.class);
+                startActivity(classesIntent);
+            }
+        });
         // we might want to load all of there info on to a local data base so it can run faster
 //        Intent classesIntent = new Intent(MainActivity.this, ClassActivity.class);
 //        startActivity(classesIntent);
