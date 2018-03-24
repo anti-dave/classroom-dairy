88bc5cb142d98c2f792b21c332422feee6e32f68
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2018-03-24 15:50:19 -0400

    update to classActivity

--- ClassManager/app/src/main/java/com/example/camdavejakerob/classmanager/ClassActivity.java
+++ ClassManager/app/src/main/java/com/example/camdavejakerob/classmanager/ClassActivity.java
@@ -5,16 +5,20 @@ import android.os.TestLooperManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+//import com.firebase.ui.database.FirebaseListAdapter;
+
 import java.util.ArrayList;
+
 
 public class ClassActivity extends AppCompatActivity {
 
-    private ClassAdapter adapter;
-    private ListView listView;
+    private ClassAdapter mAdapter;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +41,41 @@ public class ClassActivity extends AppCompatActivity {
 
         classes.add(new Class("class4", today, "13pm","2pm","os1041", 0));
 
-        adapter = new ClassAdapter(this, classes);
-        listView = (ListView) findViewById(R.id.classes);
-        listView.setAdapter(adapter);
+        //adapter = new ClassAdapter(this, classes);
 
-        final TextView dummyButton = findViewById(R.id.dummy);
-        dummyButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent dummyIntent = new Intent(ClassActivity.this, ClassInfoActivity.class);
-                startActivity(dummyIntent); // probaly should pass the class to so we can change the title but this is just a dummy
+
+        // gets ref to firebase
+        //mRef = firebase.database().ref("u0/" + "classes");//= new Firebase("https://classmanager-38435.firebaseio.com/" + "u0/" + "classes");
+        mListView = (ListView) findViewById(R.id.classes);
+//        mAdapter = new FirebaseListAdapter<Class>(this, Class.class, R.layout.activity_class, mRef) {
+//            @Override
+//            protected void populateView(View view, Class myObj, int position) {
+//                //Set the value for the views
+//                ((TextView)view.findViewById(R.id.class_name)).setText(myObj.getName());
+//                //...
+//            }
+//        };
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                // Create new intent to go to {@link EditorActivity}
+//                Intent intent = new Intent(VisitedRestaurantActivity.this, VisitedRestaurantFormActivity.class);
+//
+//                // Form the content URI that represents the specific Restaurant that was clicked on,
+//                // by appending the "id" (passed as input to this method) onto the
+//                // {@link VisitedRestaurantEntry#CONTENT_URI}.
+//                Uri currentRestaurantUri =
+//                        ContentUris.withAppendedId(VisitedRestaurantEntry.CONTENT_URI, id);
+//
+//                // Set the URI on the data field of the intent
+//                intent.setData(currentRestaurantUri);
+//
+//                // Launch the {@link EditorActivity} to display the data for the current Restaurant.
+//                startActivity(intent);
             }
         });
+
     }
 }
